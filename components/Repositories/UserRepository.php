60e3ea146679ae4958b7d2158fe7c46521e13632
Wenceslaus Dsilva <wenceslausdsilva@gmail.com>
2024-12-29 15:54:45 +0530

    #431 - Switch to PHP 8.x - 13h 0m Add UserRepository for OAuth2 user authentication
    Introduced a `UserRepository` implementation to manage user authentication with provided credentials in the OAuth2 flow. Updated `composer.lock` references to integrate the new module functionality. This addition enables streamlined user credential validation within the OAuth2 server.

---
+++ components/Repositories/UserRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace deadmantfa\yii2\oauth2\server\components\Repositories;
+
+use common\models\User;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use League\OAuth2\Server\Repositories\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+
+    /**
+     * @throws OAuthServerException
+     */
+    public function getUserEntityByUserCredentials(string $username, string $password, string $grantType, ClientEntityInterface $clientEntity): User
+    {
+
+        // Find user by username
+        $user = User::findOne(['username' => $username]);
+
+        if (!$user) {
+            throw OAuthServerException::invalidCredentials();
+        }
+
+        // Verify password
+        if (!password_verify($password, $user->password_hash)) {
+            throw OAuthServerException::invalidCredentials();
+        }
+
+        return $user;
+    }
+}
